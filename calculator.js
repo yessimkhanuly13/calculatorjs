968a37c148f8fa88baad3ea97f02712d81a6a6e3
Bekzat <esimhanuli13@gmail.com>
2023-04-03 22:35:03 +0600

    final commit

--- calculator.js
+++ calculator.js
@@ -16,22 +16,22 @@ const firstNumber = document.querySelector('#text-one');
 const secondNumber = document.querySelector('#text-two');
 const buttons = document.querySelectorAll('.buttons');
 const equalButton = document.querySelector('.equal');
+const delButton = document.querySelector('#backspace');
 let trigger = 0;
 
 buttons.forEach(function(button){
     button.addEventListener('click', ()=>{
         const key = event.target.textContent;
-
         if(operands.includes(key)){
             b = '';
             operation = true;
             sign = key;
         }
-        if(operation === false && key !== '=' && key !== '->' && !operands.includes(key)){
+        if(operation === false && key !== '=' && key !== 'DEL' && !operands.includes(key)){
             a += key;
             firstNumber.value = a;  
         }
-        if(operation === true && !operands.includes(key) && key !== '=' && key !== '->'){
+        if(operation === true && !operands.includes(key) && key !== '=' && key !== 'DEL'){
             if(trigger !== 0){
                 trigger = 0;
                 b = '';
@@ -74,20 +74,19 @@ buttons.forEach(function(button){
             secondNumber.value ='';
             clearAll();
         }
-        if(key === '->'){
+        if(key === 'DEL' ){
             if(b === ''){
-                let length = a.length;
-                if(!sign){
-                    sign = '';
-                    length += 1;
-                }
-                a = a.substring(0, length-1);
+                a = a.substring(0, a.length - 1);
                 firstNumber.value = a;
+                if(sign != null){
+                    sign = '';
+                    operation = false;
+                }
             }else{
-                b = b.substring(0, b.length-1);
+                b = b.substring(0, b.length -1);
                 secondNumber.value = b;
             }
         }
-
     })
 });
+console.log('das')
